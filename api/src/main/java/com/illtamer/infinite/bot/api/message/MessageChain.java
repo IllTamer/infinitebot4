ebc570316ac2310dccf1c26b9f360de6db515306
IllTamer <765743073@qq.com>
2022-08-07 22:49:12 +0800

    Update MessageChain.java

--- api/src/main/java/com/illtamer/infinite/bot/api/message/MessageChain.java
+++ api/src/main/java/com/illtamer/infinite/bot/api/message/MessageChain.java
@@ -1,12 +1,12 @@
 package com.illtamer.infinite.bot.api.message;
 
+import com.google.gson.Gson;
 import com.illtamer.infinite.bot.api.entity.TransferEntity;
-import com.illtamer.infinite.bot.api.entity.transfer.Image;
-import com.illtamer.infinite.bot.api.util.Maps;
+import com.illtamer.infinite.bot.api.entity.transfer.*;
+import lombok.Getter;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * Message 中所有 {@link com.illtamer.infinite.bot.api.entity.TransferEntity} 构成的链表
@@ -14,22 +14,46 @@ import java.util.Map;
 public class MessageChain {
 
     private static final Map<String, Class<? extends TransferEntity>> MAPPER;
-    private final List<TransferEntity> transferEntities;
+    @Getter
+    private final List<TransferEntity> entities;
+    private final Gson gson;
 
     protected MessageChain() {
-        this.transferEntities = new ArrayList<>();
+        this.entities = new ArrayList<>();
+        this.gson = new Gson();
     }
 
     protected void boltOn(String type, List<Map.Entry<String, Object>> data) {
-//        Gson gson = new Gson();
-//        return list.stream()
-//                .filter(entry -> entity.getName().equals(entry.key))
-//                .map(entry -> gson.fromJson(gson.toJson(entry.value), entity))
-//                .collect(Collectors.toList());
+        Class<? extends TransferEntity> clazz = MAPPER.get(type);
+        if (clazz == null)
+            clazz = Unknown.class;
+        try {
+            final Map<String, Object> objectMap = data.stream().collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+            entities.add(gson.fromJson(gson.toJson(objectMap), clazz));
+        } catch (Exception e) {
+            System.err.println("Unknown error when transferring type: " + type);
+            e.printStackTrace();
+        }
     }
 
+    @Override
+    public String toString() {
+        return "MessageChain{" +
+                "entities=" + entities +
+                '}';
+    }
+
+    // TODO 更多消息类型支持
     static {
-        MAPPER = Maps.of("image", Image.class);
+        HashMap<String, Class<? extends TransferEntity>> map = new HashMap<>();
+        map.put(At.class.getSimpleName().toLowerCase(), At.class);
+        map.put(Face.class.getSimpleName().toLowerCase(), Face.class);
+        map.put(Forward.class.getSimpleName().toLowerCase(), Forward.class);
+        map.put(Image.class.getSimpleName().toLowerCase(), Image.class);
+        map.put(Record.class.getSimpleName().toLowerCase(), Record.class);
+        map.put(Reply.class.getSimpleName().toLowerCase(), Reply.class);
+        map.put(Text.class.getSimpleName().toLowerCase(), Text.class);
+        MAPPER = map;
     }
 
 }
