957afe2413f1ce674bd462b43a644e931a2b80c4
IllTamer <765743073@qq.com>
2022-08-16 23:55:08 +0800

    Update HttpRequestUtil.java

--- api/src/main/java/com/illtamer/infinite/bot/api/util/HttpRequestUtil.java
+++ api/src/main/java/com/illtamer/infinite/bot/api/util/HttpRequestUtil.java
@@ -1,12 +1,15 @@
 package com.illtamer.infinite.bot.api.util;
 
 import com.google.gson.Gson;
+import com.illtamer.infinite.bot.api.Pair;
 import com.illtamer.infinite.bot.api.handler.QuickActionHandler;
 import lombok.SneakyThrows;
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpStatus;
+import org.apache.commons.httpclient.methods.GetMethod;
 import org.apache.commons.httpclient.methods.PostMethod;
 import org.apache.commons.httpclient.methods.StringRequestEntity;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.io.IOException;
@@ -16,13 +19,7 @@ public class HttpRequestUtil {
 
     @SneakyThrows(IOException.class)
     public static String postJson(String url, Object payload, @Nullable Map<String, String> headers) {
-        try {
-            new Gson().toJson(payload);
-        } catch (Exception e) {
-            System.err.println(((QuickActionHandler) payload).getContext());
-            System.err.println(((QuickActionHandler) payload).getOperation());
-        }
-        final String json = new Gson().toJson(payload);
+        final String payloadJson = parsePayload(payload);
         HttpClient client = new HttpClient();
         client.getHttpConnectionManager().getParams().setConnectionTimeout(3000);
         client.getHttpConnectionManager().getParams().setSoTimeout(3000);
@@ -34,12 +31,46 @@ public class HttpRequestUtil {
                     .filter(entry -> entry.getValue() != null)
                     .forEach(entry -> postMethod.setRequestHeader(entry.getKey(), entry.getValue()));
 
-        StringRequestEntity requestEntity = new StringRequestEntity(json, "application/json", "UTF-8");
+        StringRequestEntity requestEntity = new StringRequestEntity(payloadJson, "application/json", "UTF-8");
         postMethod.setRequestEntity(requestEntity);
         int status = client.executeMethod(postMethod);
         if (status == HttpStatus.SC_OK)
             return postMethod.getResponseBodyAsString();
         throw new RuntimeException("接口连接失败！");
+    }
+
+    @SneakyThrows(IOException.class)
+    public static Pair<Integer, String> getJson(String url, @Nullable Map<String, String> params) {
+        HttpClient client = new HttpClient();
+        client.getHttpConnectionManager().getParams().setConnectionTimeout(3000);
+        client.getHttpConnectionManager().getParams().setSoTimeout(3000);
+        client.getParams().setContentCharset("UTF-8");
+        GetMethod getMethod = new GetMethod(concatUrl(url, params));
+        int status = client.executeMethod(getMethod);
+        return new Pair<>(status, getMethod.getResponseBodyAsString());
+    }
+
+    @NotNull
+    public static String parsePayload(@Nullable Object payload) {
+        String content = "";
+        if (payload == null) return content;
+        try {
+            content = new Gson().toJson(payload);
+        } catch (Exception e) {
+            System.err.println(((QuickActionHandler) payload).getContext());
+            System.err.println(((QuickActionHandler) payload).getOperation());
+        }
+        return content;
+    }
+
+    @NotNull
+    private static String concatUrl(@NotNull String url, @Nullable Map<String, String> params) {
+        if (params == null || params.size() == 0) return url;
+        StringBuilder builder = new StringBuilder(url).append('?');
+        for (Map.Entry<String, String> entry : params.entrySet()) {
+            builder.append(entry.getKey()).append('=').append(entry.getValue()).append('&');
+        }
+        return builder.deleteCharAt(builder.length()-1).toString();
     }
 
 }
