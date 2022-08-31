49d204fbccadc3faa097a5488123031268fdf2cc
IllTamer <765743073@qq.com>
2022-08-31 15:18:11 +0800

    Add PluginListener

---
+++ minecraft/src/main/java/com/illtamer/infinite/bot/minecraft/listener/PluginListener.java
@@ -0,0 +1,34 @@
+package com.illtamer.infinite.bot.minecraft.listener;
+
+import com.illtamer.infinite.bot.minecraft.Bootstrap;
+import com.illtamer.infinite.bot.minecraft.api.IExternalExpansion;
+import com.illtamer.infinite.bot.minecraft.expansion.ExpansionLoader;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.server.PluginDisableEvent;
+import org.bukkit.plugin.Plugin;
+
+import java.util.logging.Logger;
+
+public class PluginListener implements Listener {
+
+    private final ExpansionLoader expansionLoader;
+    private final Logger logger;
+
+    public PluginListener(Bootstrap instance) {
+        this.expansionLoader = instance.getExpansionLoader();
+        this.logger = instance.getLogger();
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void onDisabled(PluginDisableEvent event) {
+        final Plugin plugin = event.getPlugin();
+        final IExternalExpansion externalExpansion = expansionLoader.getExternalExpansion(plugin);
+        if (externalExpansion != null) {
+            logger.warning("插件 " + plugin.getName() + " 在卸载时未主动注销附属，被动注销中...");
+            expansionLoader.disableExternalExpansion(externalExpansion);
+        }
+    }
+
+}
