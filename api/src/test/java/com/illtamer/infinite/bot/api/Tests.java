95ab11ae8790f8bc3978511d70a7d5df4d6a48eb
IllTamer <765743073@qq.com>
2023-01-07 21:01:11 +0800

    Update tests

--- api/src/test/java/com/illtamer/infinite/bot/api/Tests.java
+++ api/src/test/java/com/illtamer/infinite/bot/api/Tests.java
@@ -1,14 +1,19 @@
 package com.illtamer.infinite.bot.api;
 
+import com.illtamer.infinite.bot.api.config.CQHttpWebSocketConfiguration;
+import com.illtamer.infinite.bot.api.handler.GetMessageHandler;
+import com.illtamer.infinite.bot.api.handler.OpenAPIHandling;
 import com.illtamer.infinite.bot.api.util.HttpRequestUtil;
+
+import java.util.Map;
 
 public class Tests {
 
     public static void main(String[] args) {
-//        CQHttpWebSocketConfiguration.setHttpUri("http://47.117.136.149:5700");
-//        CQHttpWebSocketConfiguration.setAuthorization("root765743073");
+        CQHttpWebSocketConfiguration.setHttpUri("http://47.117.136.149:5700");
+        CQHttpWebSocketConfiguration.setAuthorization("root765743073");
 //
-//        System.out.println(OpenAPIHandling.getGroupMember(663950147L, 765743073L));
+        OpenAPIHandling.sendMessage("Hello World", 765743073);
 
 //        Response<Map<String, Object>> response = new GroupMsgSendHandler()
 //                .setGroupId(663950147L)
--- minecraft/src/test/java/com/illtamer/infinite/bot/minecraft/BootstrapTests.java
+++ minecraft/src/test/java/com/illtamer/infinite/bot/minecraft/BootstrapTests.java
@@ -1,42 +1,26 @@
 package com.illtamer.infinite.bot.minecraft;
 
-import com.illtamer.infinite.bot.minecraft.util.ExpansionUtil;
-
-import java.util.Arrays;
+import com.illtamer.infinite.bot.minecraft.api.IExpansion;
+import com.illtamer.infinite.bot.minecraft.expansion.automation.AutoLoadConfiguration;
 
 public class BootstrapTests {
 
-    public static void main(String[] args) {
-        System.out.println(Arrays.toString(ExpansionUtil.IDENTIFIER.split("ChatManager-2.0: IllTamer")));
-//        StackTraceElement[] elements = Thread.currentThread().getStackTrace();
-//        System.out.println(elements[elements.length - 1].getClassName());
 
-//        ThreadPoolExecutor websocketExecutor = new ThreadPoolExecutor(1, 1,
-//                0L, TimeUnit.MILLISECONDS,
-//                new LinkedBlockingQueue<>());
-//        Consumer<ThreadPoolExecutor> consumer;
-//        try {
+//    public static void main(String[] args) {
+////        CQHttpWebSocketConfiguration.setHttpUri("http://47.117.136.149:5700");
+////        CQHttpWebSocketConfiguration.setAuthorization("root765743073");
+////        System.out.println(OpenAPIHandling.getMessage(1051692076));
 //
-//            final Field field = MethodHandles.Lookup.class.getDeclaredField("IMPL_LOOKUP");
-//            field.setAccessible(true);
-//            MethodHandles.Lookup IMPL_LOOKUP = (MethodHandles.Lookup) field.get(null);
-//
-////            final MethodHandles.Lookup privateLookupIn = MethodHandles.privateLookupIn(ThreadPoolExecutor.class, MethodHandles.lookup());
-//            final MethodType methodType = MethodType.methodType(void.class);
-//            final MethodHandle methodHandle = IMPL_LOOKUP.findVirtual(ThreadPoolExecutor.class, "interruptWorkers", methodType);
-//            consumer = object -> {
-//                try {
-//                    methodHandle.invoke(object);
-//                } catch (Throwable e) {
-//                    e.printStackTrace();
-//                }
-//            };
-//        } catch (Exception e) {
-//            consumer = object -> {}; // do nothing
-//            e.printStackTrace();
-//        }
-//
-//        consumer.accept(websocketExecutor);
+//    }
+
+    static ENUM aaa;
+
+    public static void main(String[] args) throws Exception {
+        System.out.println(Enum.class.isAssignableFrom(BootstrapTests.class.getDeclaredField("aaa").getType()));
+    }
+
+    enum ENUM {
+
     }
 
 }
